65bf0e11a1d014210e836000e4c096be3668139a
Azonos712 <nekoooolay@mail.ru>
2022-07-14 13:22:48 +0300

    Solve Task1480

---
+++ LeetCodeTasks.Solutions/1_Easy/Task1480.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task1480 : ILeetCodeTask<int[]>
+    {
+        int[] input = new int[] { 1, 2, 3, 4 };
+        public int[] Invoke()
+        {
+            return RunningSum(input);
+        }
+
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+                nums[i] = nums[i] + nums[i - 1];
+
+            return nums;
+        }
+    }
+}
