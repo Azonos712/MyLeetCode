a8a8f9c50c7c787cbdd68c940748ee6bb93ba261
Azonos712 <nekoooolay@mail.ru>
2022-07-14 14:51:13 +0300

    Solve Task392

--- LeetCodeTasks.Console/Program.cs
+++ LeetCodeTasks.Console/Program.cs
@@ -11,7 +11,7 @@ namespace LeetCodeTasks.Console
     {
         static void Main(string[] args)
         {
-            ILeetCodeTask<bool> task = new Task205();
+            ILeetCodeTask<bool> task = new Task392();
             var result = task.Invoke();
             System.Console.WriteLine(string.Join(',', result));
         }
---
+++ LeetCodeTasks.Solutions/1_Easy/Task392.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task392 : ILeetCodeTask<bool>
+    {
+        string s1 = "axc";
+        string s2 = "ahbgdc";
+        public bool Invoke()
+        {
+            return IsSubsequence(s1, s2);
+        }
+
+        public bool IsSubsequence(string s, string t)
+        {
+            for (int i = 0, j = -1; i < s.Length; i++)
+            {
+                while (j < t.Length)
+                {
+                    j++;
+
+                    if (j == t.Length)
+                        return false;
+
+                    if (s[i] == t[j])
+                        break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
