3c7ad8d6c95c21526a847b8e33549f67818d2faa
Azonos712 <nekoooolay@mail.ru>
2022-07-16 15:44:06 +0300

    Solve Task142

---
+++ LeetCodeTasks.Solutions/2_Medium/Task142.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeTasks.Solutions.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._2_Medium
+{
+    public class Task142 : ILeetCodeTask<ListNode>
+    {
+        ListNode l1 = new() { val = 1, next = new() { val = 2, next = new() { val = 3, next = new() { val = 4 } } } };
+
+        public ListNode Invoke()
+        {
+            return DetectCycle(l1);
+        }
+
+        /// <summary>
+        /// Time: O(n) / Memory: O(n)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode DetectCycleWithHashTable(ListNode head)
+        {
+            HashSet<ListNode> set = new();
+
+            while (head != null)
+                if (set.Add(head))
+                    head = head.next;
+                else
+                    return head;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Time: O(n) / Memory: O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode DetectCycle(ListNode head)
+        {
+            //two pointers: slow and fast
+            return null;
+        }
+    }
+}
