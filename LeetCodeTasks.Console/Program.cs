c217d8d036c7486f9cf34dc6b577982136b93bf0
Azonos712 <nekoooolay@mail.ru>
2022-07-19 13:30:31 +0300

    Solve Task118

--- LeetCodeTasks.Console/Program.cs
+++ LeetCodeTasks.Console/Program.cs
@@ -12,7 +12,7 @@ namespace LeetCodeTasks.Console
     {
         static void Main(string[] args)
         {
-            ILeetCodeTask<int> task = new Task278();
+            ILeetCodeTask<IList<IList<int>>> task = new Task118();
             var result = task.Invoke();
             System.Console.WriteLine(string.Join(',', result));
         }
---
+++ LeetCodeTasks.Solutions/1_Easy/Task118.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task118 : ILeetCodeTask<IList<IList<int>>>
+    {
+        public IList<IList<int>> Invoke() => Generate(5);
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> rows = new List<IList<int>>();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                rows.Add(new List<int>(i + 1));
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    if (j == 0 || j == i)
+                        rows[i].Add(1);
+                    else
+                        rows[i].Add(rows[i - 1][j - 1] + rows[i - 1][j]);
+                }
+            }
+
+            return rows;
+        }
+    }
+}
