c15fa29fb9066318a61002970e46c05943860585
Azonos712 <nekoooolay@mail.ru>
2022-07-22 14:36:46 +0300

    Solve Task70

---
+++ LeetCodeTasks.Solutions/1_Easy/Task70.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task70 : ILeetCodeTask<int>
+    {
+        public int Invoke() => ClimbStairs(5);
+
+        /// <summary>
+        /// Time: O(n) / Memory: O(n)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int ClimbStairs(int n)
+        {
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                    nums[i] = 1;
+                else if (i == 1)
+                    nums[i] = 2;
+                else
+                    nums[i] = nums[i - 1] + nums[i - 2];
+            }
+            return nums[n - 1];
+        }
+    }
+}
