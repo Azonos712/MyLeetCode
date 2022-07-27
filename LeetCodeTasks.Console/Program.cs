4467e403496801ce6dc87c1c6b5c5708b7e116af
Azonos712 <nekoooolay@mail.ru>
2022-07-27 12:57:41 +0300

    Solve Task1046

--- LeetCodeTasks.Console/Program.cs
+++ LeetCodeTasks.Console/Program.cs
@@ -11,7 +11,7 @@ namespace LeetCodeTasks.Console
     {
         static void Main(string[] args)
         {
-            ILeetCodeTask<string> task = new Task394();
+            ILeetCodeTask<int> task = new Task1046();
             var result = task.Invoke();
             System.Console.WriteLine(string.Join(',', result));
         }
---
+++ LeetCodeTasks.Solutions/1_Easy/Task1046.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task1046 : ILeetCodeTask<int>
+    {
+        public int Invoke() => LastStoneWeight(new int[] { 2, 7, 4, 1, 8, 1 });
+
+        public int LastStoneWeight(int[] stones)
+        {
+            PriorityQueue<int, int> q = new PriorityQueue<int, int>();
+            for (int i = 0; i < stones.Length; i++)
+                q.Enqueue(stones[i], -stones[i]);
+
+            while (q.Count > 0)
+            {
+                if (!q.TryDequeue(out int y, out _))
+                    break;
+
+                if (!q.TryDequeue(out int x, out _))
+                    return y;
+
+                if (x != y)
+                    q.Enqueue(y - x, -(y - x));
+
+            }
+
+            return 0;
+        }
+    }
+}
