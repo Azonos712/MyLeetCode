ed0f427ad364339bea86847c8ecddc5eb964c32a
Azonos712 <nekoooolay@mail.ru>
2022-07-28 13:15:51 +0300

    Solve Task242

--- LeetCodeTasks.Console/Program.cs
+++ LeetCodeTasks.Console/Program.cs
@@ -11,7 +11,7 @@ namespace LeetCodeTasks.Console
     {
         static void Main(string[] args)
         {
-            ILeetCodeTask<IList<string>> task = new Task692();
+            ILeetCodeTask<bool> task = new Task242();
             var result = task.Invoke();
             System.Console.WriteLine(string.Join(',', result));
         }
---
+++ LeetCodeTasks.Solutions/1_Easy/Task242.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task242 : ILeetCodeTask<bool>
+    {
+        public bool Invoke() => IsAnagram("anagram", "nagaram");
+
+        public bool IsAnagram(string s, string t)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in s)
+                if (!dic.TryAdd(c, 1))
+                    dic[c]++;
+
+            foreach (char c in t)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    dic[c]--;
+                    if (dic[c] == 0)
+                        dic.Remove(c);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return dic.Count == 0;
+        }
+    }
+}
