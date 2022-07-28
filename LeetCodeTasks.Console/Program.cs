32818b9a23283de7743e55fc823894b6dae49a7a
Azonos712 <nekoooolay@mail.ru>
2022-07-28 17:55:09 +0300

    Solve Task1706

--- LeetCodeTasks.Console/Program.cs
+++ LeetCodeTasks.Console/Program.cs
@@ -11,7 +11,7 @@ namespace LeetCodeTasks.Console
     {
         static void Main(string[] args)
         {
-            ILeetCodeTask<IList<int>> task = new Task54();
+            ILeetCodeTask<int[]> task = new Task1706();
             var result = task.Invoke();
             System.Console.WriteLine(string.Join(',', result));
         }
---
+++ LeetCodeTasks.Solutions/2_Medium/Task1706.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._2_Medium
+{
+    public class Task1706 : ILeetCodeTask<int[]>
+    {
+        int[][] _grid = new int[][] {
+            new int[] {1,  1,  1, 1, 1, 1 },
+            new int[] {-1, -1, -1, -1, -1,-1 },
+            new int[] { 1, 1, 1, 1, 1, 1 },
+            new int[] { -1, -1, -1, -1, -1,-1 } };
+
+        public int[] Invoke() => FindBall(_grid);
+
+        public int[] FindBall(int[][] grid)
+        {
+            int[] result = new int[grid[0].Length];
+            for (int i = 0; i < grid[0].Length; i++)
+                result[i] = FindExit(grid, i);
+
+            return result;
+        }
+
+        int FindExit(int[][] grid, int startIndex)
+        {
+            int i = 0, j = startIndex;
+            while (i < grid.Length)
+            {
+                if (grid[i][j] == 1)
+                {
+                    if (j + 1 < grid[0].Length && grid[i][j + 1] == 1)
+                    {
+                        i++;
+                        j++;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                else if (grid[i][j] == -1)
+                {
+                    if (j - 1 >= 0 && grid[i][j - 1] == -1)
+                    {
+                        i++;
+                        j--;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return j;
+        }
+    }
+}
