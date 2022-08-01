46ccd6f5dc32994d0631af5c72fd34479b78e57c
Azonos712 <nekoooolay@mail.ru>
2022-08-01 13:30:12 +0300

    Solve Task226

--- LeetCodeTasks.Console/Program.cs
+++ LeetCodeTasks.Console/Program.cs
@@ -11,7 +11,7 @@ namespace LeetCodeTasks.Console
     {
         static void Main(string[] args)
         {
-            ILeetCodeTask<int> task = new Task621();
+            ILeetCodeTask<TreeNode> task = new Task226();
             var result = task.Invoke();
             System.Console.WriteLine(string.Join(',', result));
         }
---
+++ LeetCodeTasks.Solutions/1_Easy/Task226.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeTasks.Solutions.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task226 : ILeetCodeTask<TreeNode>
+    {
+        public TreeNode Invoke() => InvertTree(null);
+
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null || (root.left == null && root.right == null))
+                return root;
+
+            var left = InvertTree(root.left);
+            var right = InvertTree(root.right);
+
+            root.left = right;
+            root.right = left;
+
+            return root;
+        }
+    }
+}
