841445ef435d1043c3c393d569ff70d8b2ab217a
Azonos712 <nekoooolay@mail.ru>
2022-08-04 15:04:20 +0300

    Solve Task173

---
+++ LeetCodeTasks.NUnitTests/2_Medium/Task173Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NUnitTests._2_Medium
+{
+    internal class Task173Tests
+    {
+        static object[] cases =
+        {
+            new object[] { new int?[] { 7, 3, 15, null, null, 9, 20 }, new int[] { 3, 7, 9, 15, 20} },
+        };
+
+        [TestCaseSource(nameof(cases))]
+        public void Test1(int?[] nums, int[] result)
+        {
+            Task173.BSTIterator iterator = new Task173.BSTIterator(TreeNode.CreateTreeFromArray(nums));
+            List<int> list = new List<int>();
+            while (iterator.HasNext())
+                list.Add(iterator.Next());
+
+            Assert.AreEqual(list.ToArray(), result);
+        }
+    }
+}
---
+++ LeetCodeTasks.Solutions/2_Medium/Task173.cs
@@ -0,0 +1,44 @@
+﻿using LeetCodeTasks.Solutions.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._2_Medium
+{
+    public class Task173
+    {
+        public class BSTIterator
+        {
+            private Stack<TreeNode> _helper;
+
+            public BSTIterator(TreeNode root)
+            {
+                _helper = new Stack<TreeNode>();
+
+                while (root != null)
+                {
+                    _helper.Push(root);
+                    root = root.left;
+                }
+            }
+
+            public int Next()
+            {
+                TreeNode result = _helper.Pop();
+                TreeNode node = result.right;
+
+                while (node != null)
+                {
+                    _helper.Push(node);
+                    node = node.left;
+                }
+
+                return result.val;
+            }
+
+            public bool HasNext() => _helper.Count() != 0;
+        }
+    }
+}
