1c260a47404e4bb1a886dfaec371b6dae4fa9071
Azonos712 <nekoooolay@mail.ru>
2022-08-04 14:48:54 +0300

    Solve Task230

---
+++ LeetCodeTasks.NUnitTests/2_Medium/Task230Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NUnitTests._2_Medium
+{
+    internal class Task230Tests
+    {
+        readonly Task230 task = new();
+
+        static object[] cases =
+        {
+            new object[] { new int?[] { 3, 1, 4, null, 2, null, null }, 1, 1},
+            new object[] { new int?[] { 5, 3, 6, 2, 4, null, null, 1, null, null, null, null, null, null, null }, 3, 3},
+        };
+
+        [TestCaseSource(nameof(cases))]
+        public void Test1(int?[] nums, int k, int result)
+        {
+            Assert.AreEqual(task.KthSmallest(TreeNode.CreateTreeFromArray(nums), k), result);
+        }
+    }
+}
---
+++ LeetCodeTasks.Solutions/2_Medium/Task230.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeTasks.Solutions.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._2_Medium
+{
+    public class Task230
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            return Solve(root, ref k);
+        }
+
+        private int Solve(TreeNode root, ref int k)
+        {
+            if (root == null)
+                return 0;
+
+            int left = Solve(root.left, ref k);
+            if (left >= 1)
+                return left;
+            
+            k -= 1;
+            
+            if (k == 0)
+                return root.val;
+            int right = Solve(root.right, ref k);
+            return right;
+        }
+    }
+}
