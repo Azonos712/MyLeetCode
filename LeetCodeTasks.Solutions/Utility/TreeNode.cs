3b82af8e873af7ce2867fa31fe85c21c6af7148e
Azonos712 <nekoooolay@mail.ru>
2022-08-04 13:48:04 +0300

    Add array conversion to TreeNode and vice versa

--- LeetCodeTasks.Solutions/Utility/TreeNode.cs
+++ LeetCodeTasks.Solutions/Utility/TreeNode.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetCodeTasks.Solutions._1_Easy;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace LeetCodeTasks.Solutions.Utility
         public int val;
         public TreeNode left;
         public TreeNode right;
+
         public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
         {
             this.val = val;
@@ -20,6 +22,48 @@ namespace LeetCodeTasks.Solutions.Utility
             this.right = right;
         }
 
-        public override string ToString() => val.ToString();
+        public static TreeNode CreateTreeFromArray(int?[] arr, int i = 0)
+        {
+            TreeNode root = null;
+
+            if (i < arr.Length)
+            {
+                var element = arr[i];
+                if (element.HasValue)
+                {
+                    root = new TreeNode(element.Value);
+                    root.left = CreateTreeFromArray(arr, 2 * i + 1);
+                    root.right = CreateTreeFromArray(arr, 2 * i + 2);
+                }
+            }
+
+            return root;
+        }
+
+        public static int?[] CreateArrayFromTree(TreeNode node)
+        {
+            List<int?> list = new List<int?>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(node);
+            while (queue.Count != 0)
+            {
+                if (!queue.Any(x => x != null))
+                    break;
+
+                if (queue.Peek() != null)
+                    queue.Enqueue(queue.Peek().left);
+
+                if (queue.Peek() != null)
+                    queue.Enqueue(queue.Peek().right);
+
+                TreeNode element = queue.Dequeue();
+                if (element == null)
+                    list.Add(null);
+                else
+                    list.Add(element.val);
+            }
+
+            return list.ToArray();
+        }
     }
 }
