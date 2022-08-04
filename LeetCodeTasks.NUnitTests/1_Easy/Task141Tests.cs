6ca93a4746466693a8818349c38cbf1c58f63d9d
Azonos712 <nekoooolay@mail.ru>
2022-08-04 19:29:27 +0300

    Solve Task141

---
+++ LeetCodeTasks.NUnitTests/1_Easy/Task141Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NUnitTests._1_Easy
+{
+    internal class Task141Tests
+    {
+        readonly Task141 task = new();
+
+        [TestCase(new int[] { 3, 2, 0, -4 }, ExpectedResult = true)]
+        public bool Test1(int[] nums)
+        {
+            ListNode start = ListNode.CreateListFromArray(nums);
+            ListNode node = start;
+            while (node.next != null)
+                node = node.next;
+            node.next = start;
+            return task.HasCycle(start);
+        }
+
+        [TestCase(new int[] { 1, 2 }, ExpectedResult = false)]
+        [TestCase(new int[] { 1 }, ExpectedResult = false)]
+        public bool Test2(int[] nums)
+        {
+            return task.HasCycle(ListNode.CreateListFromArray(nums));
+        }
+    }
+}
---
+++ LeetCodeTasks.Solutions/1_Easy/Task141.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeTasks.Solutions.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode p1 = head, p2 = head;
+
+            while (p2 != null && p2.next != null)
+            {
+                p1 = p1.next;
+                p2 = p2.next.next;
+
+                if (p1 == p2)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
