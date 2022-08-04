c7d8b09463deb10066683d48061d170e796c9765
Azonos712 <nekoooolay@mail.ru>
2022-08-04 19:48:25 +0300

    Write tests for Task19 and update Task19 class

---
+++ LeetCodeTasks.NUnitTests/2_Medium/Task19Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NUnitTests._2_Medium
+{
+    internal class Task19Tests
+    {
+        readonly Task19 task = new();
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 2, ExpectedResult = new int[] { 1, 2, 3, 5 })]
+        [TestCase(new int[] { 1, }, 1, ExpectedResult = new int[] { })]
+        [TestCase(new int[] { 1, 2 }, 1, ExpectedResult = new int[] { 1 })]
+        public int[] Test1(int[] nums, int n)
+        {
+            return ListNode.CreateArrayFromList(task.RemoveNthFromEnd(ListNode.CreateListFromArray(nums), n));
+        }
+    }
+}
--- LeetCodeTasks.Solutions/2_Medium/Task19.cs
+++ LeetCodeTasks.Solutions/2_Medium/Task19.cs
@@ -7,37 +7,29 @@ using System.Threading.Tasks;
 
 namespace LeetCodeTasks.Solutions._2_Medium
 {
-    public class Task19 : ILeetCodeTask<ListNode>
+    public class Task19
     {
-        //ListNode _head = new ListNode() { val = 1, next = new ListNode() { val = 2, next = new ListNode() { val = 3, next = new ListNode() { val = 4, next = new ListNode() { val = 5 } } } } };
-
-        //ListNode _head = new ListNode() { val = 1, next = new ListNode() { val = 2, } };
-        //ListNode _head = new ListNode() { val = 1 };
-        ListNode _head = new ListNode() { val = 1, next = new ListNode() { val = 2 } };
-
-        public ListNode Invoke() => RemoveNthFromEnd(_head, 2);
-
         public ListNode RemoveNthFromEnd(ListNode head, int n)
         {
             int counter = 0;
-            ListNode result = head;
-            ListNode marked = head;
+            ListNode right = head;
+            ListNode left = head;
 
-            while (head != null)
+            while (right != null)
             {
                 if (counter > n)
-                    marked = marked.next;
+                    left = left.next;
 
-                head = head.next;
+                right = right.next;
                 counter++;
             }
 
             if (counter == n)
-                result = result.next;
-            else
-                marked.next = marked.next.next;
+                return head.next;
 
-            return result;
+            left.next = left.next.next;
+
+            return head;
         }
     }
 }
