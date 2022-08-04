b56c1a6eab3994eaced3f20fa01612f732be00e3
Azonos712 <nekoooolay@mail.ru>
2022-08-04 13:59:55 +0300

    Add tests for ListNode and TreeNode classes

---
+++ LeetCodeTasks.NUnitTests/Utility/ListNodeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NUnitTests.Utility
+{
+    internal class ListNodeTests
+    {
+        [TestCase(new int[] { 2, 4, 3 }, ExpectedResult = new int[] { 2, 4, 3 })]
+        [TestCase(new int[] { 0 }, ExpectedResult = new int[] { 0 })]
+        [TestCase(new int[] { }, ExpectedResult = new int[] { })]
+        public int[] Test1(int[] arr)
+        {
+            return ListNode.CreateArrayFromList(ListNode.CreateListFromArray(arr));
+        }
+    }
+}
---
+++ LeetCodeTasks.NUnitTests/Utility/TreeNodeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NUnitTests.Utility
+{
+    internal class TreeNodeTests
+    {
+        static object[] cases =
+        {
+            new object[] { new int?[] { 2, 4, 3 }, new int?[] { 2, 4, 3 }},
+            new object[] { new int?[] { 0 }, new int?[] { 0 }},
+            new object[] { new int?[] { 1, 2, 3, 4, null, null, 5 }, new int?[] { 1, 2, 3, 4, null, null, 5 } },
+            new object[] { new int?[] { }, new int?[] { } }
+        };
+
+        [TestCaseSource(nameof(cases))]
+        public void Test1(int?[] arr1, int?[] arr2)
+        {
+            Assert.That(arr2, Is.EqualTo(TreeNode.CreateArrayFromTree(TreeNode.CreateTreeFromArray(arr1))));
+        }
+    }
+}
