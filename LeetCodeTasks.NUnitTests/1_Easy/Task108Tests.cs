385b2db2d261a2a0c158510c312352b604c28161
Azonos712 <nekoooolay@mail.ru>
2022-08-04 14:20:04 +0300

    Solve Task108

---
+++ LeetCodeTasks.NUnitTests/1_Easy/Task108Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NUnitTests._1_Easy
+{
+    internal class Task108Tests
+    {
+        readonly Task108 task = new();
+
+        static object[] cases =
+        {
+            new object[] { new int[] { 2, 3, 4 }, new int?[] { 3, 2, 4 }},
+            new object[] { new int[] { 0 }, new int?[] { 0 }},
+            new object[] { new int[] { -10, -3, 0, 5, 9 }, new int?[] { 0, -10, 5, null, -3, null, 9 } },
+            new object[] { new int[] { 1, 3 }, new int?[] { 1, null, 3 } }
+        };
+
+        [TestCaseSource(nameof(cases))]
+        public void Test1(int[] nums, int?[] result)
+        {
+            Assert.AreEqual(TreeNode.CreateArrayFromTree(task.SortedArrayToBST(nums)), result);
+        }
+    }
+}
---
+++ LeetCodeTasks.Solutions/1_Easy/Task108.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeTasks.Solutions.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Solutions._1_Easy
+{
+    public class Task108
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return sortedBST(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode sortedBST(int[] nums, int low, int high)
+        {
+            if (low > high) return null;
+            int temp = (low + high) / 2;
+            TreeNode node = new TreeNode(nums[temp]);
+            node.left = sortedBST(nums, low, temp - 1);
+            node.right = sortedBST(nums, temp + 1, high);
+            return node;
+        }
+    }
+}
